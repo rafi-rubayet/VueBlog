b75f904d3fcae9de99feaaeb0bbeb6418d44d23c
rafirubayet <rafi.rubayet.jp@gmail.com>
2024-12-16 11:26:24 +0900

    Create Guest.vue

---
+++ resources/js/layouts/Guest.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div>
+            <a href="/">
+                <img src="../../../public/RR_LOGO_2_rbg.png" alt="VueBLog" class="w-[60px] h-[75px]" />
+            </a>
+        </div>
+
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
